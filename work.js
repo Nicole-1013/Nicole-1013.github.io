8c71b400428a53c32e532048a3af0d9198516a88
Nicole-1013 <ngalvan1013@gmail.com>
2025-04-25 14:24:33 -0700

    project sec

--- work.js
+++ work.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
+$(function(){
     const images = [
         "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg",
         "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvssaSOvzMoStZaY8YL5boN6Cz3pPz_sbF5w&s",
     ];
 
-    const revolvingImage = document.getElementById("revolving-image");
+    const revolvingImage = $("#revolving-image");
     
     if (!revolvingImage) {
         console.error("Could not find the #revolving-image element.");
@@ -17,8 +17,9 @@ document.addEventListener("DOMContentLoaded", () => {
     function changeImage() {
         currentImageIndex = (currentImageIndex + 1) % images.length;
         console.log(`Changing image to: ${images[currentImageIndex]}`); // Debugging
-        revolvingImage.src = images[currentImageIndex];
+        revolvingImage.attr("src", images[currentImageIndex]);
     }
 
     setInterval(changeImage, 5000);
 });
+
